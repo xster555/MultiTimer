b5a280e6b8f7868484cb4883c628b5e82a5d0e2e
Xster <xhwang950518@gmail.com>
2024-03-20 10:30:55 +0800

    save to eeprom

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ TFT_eSprite countDownSprite = TFT_eSprite(&tft);
 TFT_eSprite leftButtonSprite = TFT_eSprite(&tft);
 TFT_eSprite rightButtonSprite = TFT_eSprite(&tft);
 
+TFT_eSprite logoSprite = TFT_eSprite(&tft);
+
 OneButton leftButton(LEFT_BTN, true);
 OneButton rightButton(RIGHT_BTN, true);
 
@@ -38,6 +40,7 @@ long flashScreenCurrMillis = 0;
 
 bool shouldFlashScreen = false;
 
+/** 按键事件处理相关 **/
 void handleLeftButtonClick()
 {
   Serial.println("Left button clicked");
@@ -72,13 +75,16 @@ void handleLeftButtonLongPressStart()
     setupButtonSprites();
   }
 }
+
 void handleLeftButtonLongPressStop()
 {
   if (timerState == 0)
   {
     decreaseSelected = false;
+    saveData();
   }
 }
+
 void handleRightButtonClick()
 {
   Serial.println("Right button clicked");
@@ -121,15 +127,60 @@ void handleRightButtonLongPressStart()
       increaseSelected = true;
   }
 }
+
 void handleRightButtonLongPressStop()
 {
   if (timerState == 0)
   {
     increaseSelected = false;
+    saveData();
   }
 }
+/** 按键事件处理相关 end **/
 
-void setupButtons()
+// 初始化菜单Sprite
+void setupMenuSprites()
+{
+  setWorkTimeSprite.createSprite(TFT_WIDTH, 100);
+  setRestTimeSprite.createSprite(TFT_WIDTH, 100);
+  setTotalRepsSprite.createSprite(TFT_WIDTH, 100);
+
+  setWorkTimeSprite.setTextDatum(MC_DATUM);
+  setRestTimeSprite.setTextDatum(MC_DATUM);
+  setTotalRepsSprite.setTextDatum(MC_DATUM);
+
+  updateMenuItem();
+}
+
+// 移除菜单Sprite
+void removeMenuSprites()
+{
+  setWorkTimeSprite.deleteSprite();
+  setRestTimeSprite.deleteSprite();
+  setTotalRepsSprite.deleteSprite();
+  tft.fillScreen(TFT_DARKGREY);
+}
+
+// 初始化菜单界面的按键
+void setupMenuButtons()
+{
+  leftButtonSprite.fillSprite(TFT_DARKGREY);
+  leftButtonSprite.setTextDatum(MC_DATUM);
+  leftButtonSprite.setTextColor(TFT_BLACK, TFT_DARKGREY);
+  leftButtonSprite.setTextSize(2);
+  leftButtonSprite.drawString("UP", leftButtonSprite.width() / 2, leftButtonSprite.height() / 2);
+  leftButtonSprite.pushSprite(0, TFT_HEIGHT - 20);
+
+  rightButtonSprite.fillSprite(TFT_DARKGREY);
+  rightButtonSprite.setTextDatum(MC_DATUM);
+  rightButtonSprite.setTextColor(TFT_BLACK, TFT_DARKGREY);
+  rightButtonSprite.setTextSize(2);
+  rightButtonSprite.drawString("DN/GO", rightButtonSprite.width() / 2, rightButtonSprite.height() / 2);
+  rightButtonSprite.pushSprite(TFT_WIDTH - rightButtonSprite.width(), TFT_HEIGHT - 20);
+}
+
+// 配置按键事件
+void setupButtonEvents()
 {
   leftButton.attachClick(handleLeftButtonClick);
   leftButton.attachDoubleClick(handleLeftButtonDoubleClick);
@@ -143,28 +194,7 @@ void setupButtons()
   rightButton.setLongPressIntervalMs(800);
 }
 
-void updateCountInfo()
-{
-  countInfoSprite.fillSprite(TFT_BLACK);
-  countInfoSprite.drawString(String(currentRep) + " / " + String(totalReps), 10, 10);
-  countInfoSprite.pushSprite(0, 0);
-}
-
-void setupCountDownSprites()
-{
-  countInfoSprite.createSprite(TFT_WIDTH, 50);
-  countDownSprite.createSprite(TFT_WIDTH, 100);
-
-  countInfoSprite.setTextSize(3);
-  countInfoSprite.drawString("Get Ready", 3, 10);
-  countInfoSprite.pushSprite(0, 0);
-
-  countDownSprite.setTextSize(10);
-  countDownSprite.setTextDatum(MC_DATUM);
-  countDownSprite.drawString(String(preCountDown), countDownSprite.width() / 2, countDownSprite.height() / 2 + 2);
-  countDownSprite.pushSprite(0, 50);
-}
-
+// 更新菜单项
 void updateMenuItem()
 {
   Serial.println("Updating menu item: " + String(menuItem));
@@ -215,27 +245,40 @@ void updateMenuItem()
   setTotalRepsSprite.pushSprite(0, 200);
 }
 
-void setupMenuSprites()
+// 更新计数器信息栏
+void updateCountInfo()
 {
-  setWorkTimeSprite.createSprite(TFT_WIDTH, 100);
-  setRestTimeSprite.createSprite(TFT_WIDTH, 100);
-  setTotalRepsSprite.createSprite(TFT_WIDTH, 100);
-
-  setWorkTimeSprite.setTextDatum(MC_DATUM);
-  setRestTimeSprite.setTextDatum(MC_DATUM);
-  setTotalRepsSprite.setTextDatum(MC_DATUM);
-
-  updateMenuItem();
+  countInfoSprite.fillSprite(TFT_BLACK);
+  countInfoSprite.drawString(String(currentRep) + " / " + String(totalReps), 10, 10);
+  countInfoSprite.pushSprite(0, 0);
 }
 
-void removeMenuSprites()
+// 初始化计数器Sprite
+void setupCountDownSprites()
 {
-  setWorkTimeSprite.deleteSprite();
-  setRestTimeSprite.deleteSprite();
-  setTotalRepsSprite.deleteSprite();
-  tft.fillScreen(TFT_DARKGREY);
+  logoSprite.createSprite(TFT_WIDTH, 55);
+  logoSprite.fillSprite(TFT_DARKGREY);
+  logoSprite.setTextColor(TFT_WHITE, TFT_DARKGREY);
+  logoSprite.setTextSize(2);
+  logoSprite.drawString("Designed by", 5, 5);
+  logoSprite.setTextSize(3);
+  logoSprite.drawString("XSTER", 5, 30);
+  logoSprite.pushSprite(0, 220);
+
+  countInfoSprite.createSprite(TFT_WIDTH, 50);
+  countDownSprite.createSprite(TFT_WIDTH, 170);
+
+  countInfoSprite.setTextSize(3);
+  countInfoSprite.drawString("Get Ready", 3, 10);
+  countInfoSprite.pushSprite(0, 0);
+
+  countDownSprite.setTextSize(10);
+  countDownSprite.setTextDatum(MC_DATUM);
+  countDownSprite.drawString(String(preCountDown), countDownSprite.width() / 2, countDownSprite.height() / 2 + 2);
+  countDownSprite.pushSprite(0, 50);
 }
 
+// 初始化按键Sprite
 void setupButtonSprites()
 {
   leftButtonSprite.createSprite(65, 20);
@@ -243,6 +286,7 @@ void setupButtonSprites()
   setupMenuButtons();
 }
 
+// 初始化结束界面的按键
 void setupFinishButtons()
 {
   leftButtonSprite.fillSprite(TFT_RED);
@@ -256,6 +300,7 @@ void setupFinishButtons()
   rightButtonSprite.pushSprite(TFT_WIDTH - rightButtonSprite.width(), TFT_HEIGHT - 20);
 }
 
+// 更新运行界面的按键状态
 void updateRunButtons()
 {
   rightButtonSprite.setTextDatum(MC_DATUM);
@@ -276,23 +321,7 @@ void updateRunButtons()
   rightButtonSprite.pushSprite(TFT_WIDTH - rightButtonSprite.width(), TFT_HEIGHT - 20);
 }
 
-void setupMenuButtons()
-{
-  leftButtonSprite.fillSprite(TFT_DARKGREY);
-  leftButtonSprite.setTextDatum(MC_DATUM);
-  leftButtonSprite.setTextColor(TFT_BLACK, TFT_DARKGREY);
-  leftButtonSprite.setTextSize(2);
-  leftButtonSprite.drawString("UP", leftButtonSprite.width() / 2, leftButtonSprite.height() / 2);
-  leftButtonSprite.pushSprite(0, TFT_HEIGHT - 20);
-
-  rightButtonSprite.fillSprite(TFT_DARKGREY);
-  rightButtonSprite.setTextDatum(MC_DATUM);
-  rightButtonSprite.setTextColor(TFT_BLACK, TFT_DARKGREY);
-  rightButtonSprite.setTextSize(2);
-  rightButtonSprite.drawString("DOWN", rightButtonSprite.width() / 2, rightButtonSprite.height() / 2);
-  rightButtonSprite.pushSprite(TFT_WIDTH - rightButtonSprite.width(), TFT_HEIGHT - 20);
-}
-
+// 初始化运行界面的按键
 void setupRunButtons()
 {
   leftButtonSprite.fillSprite(TFT_RED);
@@ -310,6 +339,7 @@ void setupRunButtons()
   rightButtonSprite.pushSprite(TFT_WIDTH - rightButtonSprite.width(), TFT_HEIGHT - 20);
 }
 
+// 更新计数器，结束界面
 void updateCounterFinish()
 {
   countDownSprite.fillSprite(TFT_GREEN);
@@ -317,6 +347,8 @@ void updateCounterFinish()
   countDownSprite.drawString("DONE", countDownSprite.width() / 2 + 3, countDownSprite.height() / 2 + 2);
   countDownSprite.pushSprite(0, 50);
 }
+
+// 更新计数器，运行界面
 void updateCounter()
 {
   Serial.println("Updating counter");
@@ -329,6 +361,8 @@ void updateCounter()
   countDownSprite.drawString(String(currentCount), countDownSprite.width() / 2, countDownSprite.height() / 2 + 2);
   countDownSprite.pushSprite(0, 50);
 }
+
+// 更新计数器，运行界面反色
 void updateCounterInvert()
 {
   countDownSprite.fillSprite(TFT_DARKGREY);
@@ -337,16 +371,54 @@ void updateCounterInvert()
   countDownSprite.pushSprite(0, 50);
 }
 
+// 加载EEPROM数据
+void loadData()
+{
+  EEPROM.begin(4096);
+  workTime = EEPROM.read(0);
+  if (workTime < MIN_WORK_TIME)
+    workTime = MIN_WORK_TIME;
+  else if (workTime > MAX_WORK_TIME)
+    workTime = MAX_WORK_TIME;
+
+  restTime = EEPROM.read(1);
+  if (restTime < MIN_REST_TIME)
+    restTime = MIN_REST_TIME;
+  else if (restTime > MAX_REST_TIME)
+    restTime = MAX_REST_TIME;
+
+  totalReps = EEPROM.read(2);
+  if (totalReps < MIN_REPS)
+    totalReps = MIN_REPS;
+  else if (totalReps > MAX_REPS)
+    totalReps = MAX_REPS;
+
+  EEPROM.end();
+}
+
+// 保存数据到EEPROM
+void saveData()
+{
+  EEPROM.begin(4096);
+  EEPROM.write(0, workTime);
+  EEPROM.write(1, restTime);
+  EEPROM.write(2, totalReps);
+  EEPROM.commit();
+  EEPROM.end();
+}
+
 void setup()
 {
   Serial.begin(115200);
+
+  loadData();
 
   tft.init();
   tft.fillScreen(TFT_DARKGREY);
 
   setupMenuSprites();
   setupButtonSprites();
-  setupButtons();
+  setupButtonEvents();
 }
 
 void loop()
@@ -368,22 +440,22 @@ void loop()
       if (menuItem == 0)
       {
         workTime++;
-        if (workTime > 99)
-          workTime = 99;
+        if (workTime > MAX_WORK_TIME)
+          workTime = MAX_WORK_TIME;
         updateMenuItem();
       }
       else if (menuItem == 1)
       {
         restTime++;
-        if (restTime > 99)
-          restTime = 99;
+        if (restTime > MAX_REST_TIME)
+          restTime = MAX_REST_TIME;
         updateMenuItem();
       }
       else if (menuItem == 2)
       {
         totalReps++;
-        if (totalReps > 99)
-          totalReps = 99;
+        if (totalReps > MAX_REPS)
+          totalReps = MAX_REPS;
         updateMenuItem();
       }
     }
@@ -392,22 +464,22 @@ void loop()
       if (menuItem == 0)
       {
         workTime--;
-        if (workTime < 5)
-          workTime = 5;
+        if (workTime < MIN_WORK_TIME)
+          workTime = MIN_WORK_TIME;
         updateMenuItem();
       }
       else if (menuItem == 1)
       {
         restTime--;
-        if (restTime < 5)
-          restTime = 5;
+        if (restTime < MIN_REST_TIME)
+          restTime = MIN_REST_TIME;
         updateMenuItem();
       }
       else if (menuItem == 2)
       {
         totalReps--;
-        if (totalReps < 1)
-          totalReps = 1;
+        if (totalReps < MIN_REPS)
+          totalReps = MIN_REPS;
         updateMenuItem();
       }
     }
--- src/main.h
+++ src/main.h
@@ -2,21 +2,63 @@
 #include "TFT_eSPI.h"
 #include <OneButton.h>
 #include <SPI.h>
+#include <EEPROM.h>
 
 #define LEFT_BTN 0
 #define RIGHT_BTN 14
 
+#define MIN_WORK_TIME 5
+#define MAX_WORK_TIME 99
+#define MIN_REST_TIME 0
+#define MAX_REST_TIME 99
+#define MIN_REPS 1
+#define MAX_REPS 99
+
 /** 按键事件处理相关 **/
+/*
+ * state == 0 选择上一菜单项
+ */
 void handleLeftButtonClick();
 void handleLeftButtonDoubleClick();
+/*
+ * state == 0 菜单项对应值 -1 开始
+ * state == 1|2|3 返回菜单页面
+ */
 void handleLeftButtonLongPressStart();
+/*
+ * state == 0 菜单项对应值 -1 停止
+ */
 void handleLeftButtonLongPressStop();
+/*
+ * state == 0 选择下一菜单项
+ * state == 1 暂停倒计时
+ * state == 2 开始倒计时
+ */
 void handleRightButtonClick();
+/*
+ * state == 0 开始倒计时
+ */
 void handleRightButtonDoubleClick();
+/*
+ * state == 0 菜单项对应值 +1 开始
+ */
 void handleRightButtonLongPressStart();
+/*
+ * state == 0 菜单项对应值 +1 停止
+ */
 void handleRightButtonLongPressStop();
 /** 按键事件处理相关 end **/
 
+// 初始化菜单Sprite
+void setupMenuSprites();
+// 移除菜单Sprite
+void removeMenuSprites();
+// 更新菜单项
+void updateMenuItem();
+// 初始化菜单界面的按键
+void setupMenuButtons();
+// 配置按键事件
+void setupButtonEvents();
 // 更新计数器，结束界面
 void updateCounterFinish();
 // 更新计数器，运行界面
@@ -25,19 +67,19 @@ void updateCounter();
 void updateCounterInvert();
 // 更新计数器信息栏
 void updateCountInfo();
-// 更新菜单项
-void updateMenuItem();
 // 更新运行界面的按键状态
 void updateRunButtons();
-// 初始化菜单界面的按键
-void setupMenuButtons();
 // 初始化运行界面的按键
 void setupRunButtons();
 // 初始化结束界面的按键
 void setupFinishButtons();
-
+// 初始化按键Sprite
 void setupButtonSprites();
+// 初始化计数器Sprite
 void setupCountDownSprites();
+// 移除计数器Sprite
 void removeCountDownSprites();
-void setupMenuSprites();
-void removeMenuSprites();
+// 加载EEPROM数据
+void loadData();
+// 保存数据到EEPROM
+void saveData();
